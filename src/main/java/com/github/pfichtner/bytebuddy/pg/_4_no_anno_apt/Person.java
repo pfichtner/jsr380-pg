61dd6682c7902c01d90a13a3c140741f8c63b7b1
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-10-20 09:17:52 +0200

    purged lombok for security reasons

--- src/main/java/com/github/pfichtner/bytebuddy/pg/_4_no_anno_apt/Person.java
+++ src/main/java/com/github/pfichtner/bytebuddy/pg/_4_no_anno_apt/Person.java
@@ -1,19 +1,47 @@
 package com.github.pfichtner.bytebuddy.pg._4_no_anno_apt;
 
-import static lombok.AccessLevel.PRIVATE;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import lombok.Value;
-import lombok.experimental.FieldDefaults;
-
-@Value
-@FieldDefaults(level = PRIVATE, makeFinal = true)
 public class Person {
 
 	@NotNull
 	@Size(min = 2, max = 20)
-	String name;
+	private final String name;
+
+	public Person(@NotNull @Size(min = 2, max = 20) String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
 
 }
